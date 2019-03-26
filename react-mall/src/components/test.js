687a8e8eec5d40d2560fc4800bf9d642b54c2428
chenying <ying.chen@huangjinqianbao.com>
2019-03-26 15:51:10 +0800

    ADD 公共文件 文件夹

---
+++ react-mall/src/components/test.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class test extends Component {
+  render() {
+    return (
+      <div>
+         我是组件test
+      </div>
+    );
+  }
+}
+
+export default test;
