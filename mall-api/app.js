6ab839b4f52b8403719a3182a3af3f7c9b687a36
xiaochen_jia <HZ08927@cn.utstarcom.com>
2019-04-01 09:09:45 +0800

    MOD注释

--- mall-api/app.js
+++ mall-api/app.js
@@ -21,12 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/search', searchRouter);
-app.use('/cart', cartRouter);
-app.use('/user', userRouter);
-
-//原因:处理跨域请求
+//原因:处理跨域请求,必须放在这个位置，不能放在下列请求的下边
 app.all('*', function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   //Access-Control-Allow-Headers ,可根据浏览器的F12查看,把对应的粘贴在这里就行
@@ -35,6 +30,13 @@ app.all('*', function (req, res, next) {
   res.header('Content-Type', 'application/json;charset=utf-8');
   next();
 });
+
+app.use('/', indexRouter);
+app.use('/search', searchRouter);
+app.use('/cart', cartRouter);
+app.use('/user', userRouter);
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
